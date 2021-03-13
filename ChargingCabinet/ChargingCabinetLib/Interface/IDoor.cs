d4f7f0bffa63480bf18ca45026e9f5a89f6a5c01
Gaiashealer <73889630+Gaiashealer@users.noreply.github.com>
2021-03-13 11:36:29 +0100

    IDoor event crated

--- ChargingCabinet/ChargingCabinetLib/Interface/IDoor.cs
+++ ChargingCabinet/ChargingCabinetLib/Interface/IDoor.cs
@@ -1,8 +1,14 @@
-﻿namespace ChargingCabinetLib.Interface
-{
+﻿using System;
 
+namespace ChargingCabinetLib.Interface
+{
+    public class DoorOpenEventArgs : EventArgs
+    {
+        public int DoorOpen { set; get; }
+    }
     public interface IDoor
     {
+        event EventHandler<DoorOpenEventArgs> DoorOpenCloseEvent;
         void OnDoorOpen();
         void OnDoorClose();
 
