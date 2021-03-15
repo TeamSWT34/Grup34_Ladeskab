272deb7d7d591f5ff1048fe2ac1f6fd162417cd8
TheGuardiaan <casperfevre@gmail.com>
2021-03-15 19:52:22 +0100

    Unit test on UsbCharger for start and stop charger

--- ChargingCabinet/ChargingCabinetLib.Test.Unit/ChargerControlTests.cs
+++ ChargingCabinet/ChargingCabinetLib.Test.Unit/ChargerControlTests.cs
@@ -1,10 +1,43 @@
-﻿using NUnit.Framework;
+﻿using ChargingCabinetLib.Interface;
+using NSubstitute;
+using NUnit.Framework;
 
 namespace ChargingCabinetLib.Test.Unit
 {
     [TestFixture]
     public class ChargerControlTests
     {
-        
+        private ChargerControl _uut;
+        private IUsbCharger _fakeUsbCharger;
+        private IChargerDisplay _fakeDisplay;
+        [SetUp]
+        public void Setup()
+        {
+            _fakeUsbCharger = Substitute.For<IUsbCharger>();
+            _fakeDisplay = Substitute.For<IChargerDisplay>();
+            _uut = new ChargerControl(_fakeUsbCharger, _fakeDisplay);
+        }
+
+
+        [Test]
+        public void StartCharge_UsbCharger_Received()
+        {
+            _uut.StartCharge();
+            _fakeUsbCharger.Received().StartCharge();
+        }
+
+        [Test]
+        public void StopCharge_UsbCharger_Received()
+        {
+            _uut.StopCharge();
+            _fakeUsbCharger.Received().StopCharge();
+        }
+
+        //[TestCase("500")]
+        //public void DisplayCounterValue_ChargerDiplay(string chargeMsg)
+        //{
+        //    _uut.StartCharge();
+        //    _fakeDisplay.Received().DisplayStationMsg();
+        //}
     }
-}
+} 
