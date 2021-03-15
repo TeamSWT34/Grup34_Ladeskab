95a0b3dc75e06b321dbda0b0c8cfe6749d82ab33
Gaiashealer <73889630+Gaiashealer@users.noreply.github.com>
2021-03-15 19:05:35 +0100

    FileLoggerTest test -> does not throw exception

--- ChargingCabinet/ChargingCabinetLib.Test.Unit/FileLoggerTests.cs
+++ ChargingCabinet/ChargingCabinetLib.Test.Unit/FileLoggerTests.cs
@@ -5,6 +5,18 @@ namespace ChargingCabinetLib.Test.Unit
     [TestFixture]
     public class FileLoggerTests
     {
-        
+        private FileLogger _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new FileLogger();
+        }
+
+        [Test]
+        public void ConsoleChargeDisplay_CanRunDisplayProgramMsg()
+        {
+            Assert.DoesNotThrow(() => _uut.Log("test"));
+        }
     }
 }
