f389d6ff609b49b113a3c77c1ccf39cf15866db4
Gaiashealer <73889630+Gaiashealer@users.noreply.github.com>
2021-03-15 19:02:46 +0100

    Some tests for ConsoleDisplayTests

--- ChargingCabinet/ChargingCabinetLib.Test.Unit/ConsoleChargerDisplayTests.cs
+++ ChargingCabinet/ChargingCabinetLib.Test.Unit/ConsoleChargerDisplayTests.cs
@@ -1,10 +1,36 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NSubstitute;
+using NUnit.Framework;
 
 namespace ChargingCabinetLib.Test.Unit
 {
     [TestFixture]
     public class ConsoleChargerDisplayTests
     {
-        
+        private ConsoleChargerDisplay _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new ConsoleChargerDisplay();
+        }
+
+        [Test]
+        public void ConsoleChargeDisplay_CanRunDisplayProgramMsg()
+        {
+            Assert.DoesNotThrow(()=>_uut.DisplayStationMsg("test"));
+        }
+
+        [Test]
+        public void ConsoleChargeDisplay_CanRunDisplayStationMsg()
+        {
+            Assert.DoesNotThrow(() => _uut.DisplayStationMsg("test"));
+        }
+
+        [Test]
+        public void ConsoleChargeDisplay_CanRunDisplayChargerMsg()
+        {
+            Assert.DoesNotThrow(() => _uut.DisplayStationMsg("test"));
+        }
     }
 }
