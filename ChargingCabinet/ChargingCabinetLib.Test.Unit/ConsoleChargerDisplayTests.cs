352196e2a75eba580e5329b5c3046eb8dc8aa2e6
Gaiashealer <73889630+Gaiashealer@users.noreply.github.com>
2021-03-18 15:57:48 +0100

    Test for Display functions in ConsoleChargerDisplay

--- ChargingCabinet/ChargingCabinetLib.Test.Unit/ConsoleChargerDisplayTests.cs
+++ ChargingCabinet/ChargingCabinetLib.Test.Unit/ConsoleChargerDisplayTests.cs
@@ -8,6 +8,13 @@ namespace ChargingCabinetLib.Test.Unit
     [TestFixture]
     public class ConsoleChargerDisplayTests
     {
+        private const string StationBeginingMsg = "Station msg : ";
+        private const string ChargerBeginingMsg = "Charge msg : ";
+
+        private const string ProgramIniMsg = "started";
+        private const string StationIniMsg = "ini";
+        private const string ChargerIniMsg = "none";
+
         private ConsoleChargerDisplay _uut;
         private IConsoleControl _fakConsoleControl;
 
@@ -24,6 +31,20 @@ namespace ChargingCabinetLib.Test.Unit
         public void DisplayProgramMsg_CanRun(string textIn)
         {
             _uut.DisplayProgramMsg(textIn);
+
+            _fakConsoleControl.Received(3).SetCursorPosition(Arg.Any<int>(),Arg.Any<int>());
+            
+            _fakConsoleControl.Received(1).WriteLine(ProgramIniMsg);
+
+            _fakConsoleControl.Received(2).WriteLine(StationBeginingMsg+StationIniMsg);
+
+            _fakConsoleControl.Received(2).WriteLine(ChargerBeginingMsg + ChargerIniMsg);
+
+            _fakConsoleControl.Received(1).WriteLine(textIn);
+
+            _fakConsoleControl.Received(2).WriteLine();
+
+            _fakConsoleControl.Received(10).WriteLine(Arg.Any<string>());
         }
 
         [TestCase("test")]
@@ -32,6 +53,21 @@ namespace ChargingCabinetLib.Test.Unit
         public void DisplayStationMsg_CanRun(string textIn)
         {
             _uut.DisplayStationMsg(textIn);
+
+            _fakConsoleControl.Received(3).SetCursorPosition(Arg.Any<int>(), Arg.Any<int>());
+
+            _fakConsoleControl.Received(2).WriteLine(ProgramIniMsg);
+
+            _fakConsoleControl.Received(1).WriteLine(StationBeginingMsg + StationIniMsg);
+
+            _fakConsoleControl.Received(2).WriteLine(ChargerBeginingMsg + ChargerIniMsg);
+
+            _fakConsoleControl.Received(1).WriteLine(StationBeginingMsg + textIn);
+
+            _fakConsoleControl.Received(2).WriteLine();
+
+            _fakConsoleControl.Received(10).WriteLine(Arg.Any<string>());
+            
         }
 
         [TestCase("test")]
@@ -40,6 +76,22 @@ namespace ChargingCabinetLib.Test.Unit
         public void DisplayChargerMsg_CanRun(string textIn)
         {
             _uut.DisplayChargerMsg(textIn);
+
+            _fakConsoleControl.Received(3).SetCursorPosition(Arg.Any<int>(), Arg.Any<int>());
+
+            _fakConsoleControl.Received(2).WriteLine(ProgramIniMsg);
+
+            _fakConsoleControl.Received(2).WriteLine(StationBeginingMsg + StationIniMsg);
+
+            _fakConsoleControl.Received(1).WriteLine(ChargerBeginingMsg + ChargerIniMsg);
+
+            _fakConsoleControl.Received(1).WriteLine(ChargerBeginingMsg + textIn);
+
+            _fakConsoleControl.Received(2).WriteLine();
+
+            _fakConsoleControl.Received(10).WriteLine(Arg.Any<string>());
         }
+
+
     }
 }
