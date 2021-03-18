2fd477eb15f108693fbdac7240ca376176b0cc1f
Gaiashealer <73889630+Gaiashealer@users.noreply.github.com>
2021-03-18 13:58:33 +0100

    Created Implementaion of ConsoleControl

---
+++ ChargingCabinet/ChargingCabinetLib/ConsoleControl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ChargingCabinetLib.Interface;
+
+namespace ChargingCabinetLib
+{
+    public class ConsoleControl : IConsoleControl
+    {
+        public int WindowWidth => Console.WindowWidth;
+        public int CursorLeft => Console.CursorLeft;
+        public int CursorTop => Console.CursorTop;
+        public void SetCursorPosition(int left, int top) => Console.SetCursorPosition(left, top);
+        public void WriteLine(string text) => Console.WriteLine(text);
+        public string ReadLine() => Console.ReadLine();
+
+    }
+}
---
+++ ChargingCabinet/ChargingCabinetLib/Interface/IConsoleControl.cs
@@ -0,0 +1,12 @@
+﻿namespace ChargingCabinetLib.Interface
+{
+    internal interface IConsoleControl
+    {
+        int WindowWidth { get; }
+        int CursorLeft { get; }
+        int CursorTop { get; }
+        void SetCursorPosition(int left, int top);
+        void WriteLine(string text);
+        string ReadLine();
+    }
+}
